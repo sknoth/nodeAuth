8a0b46b68b92ad47be1da634ca8fdf2d8331527d
Stefanie Knoth <Stefanie Knoth>
2016-09-24 10:30:06 +0200

    initial commit

---
+++ config/auth.js
@@ -0,0 +1,19 @@
+// expose our config directly to our application using module.exports
+module.exports = {
+    'facebookAuth' : {
+        'clientID'      : '1585381351768665', // your App ID
+        'clientSecret'  : '01825b87881c156f574bde370f4ed0f5', // your App Secret
+        'callbackURL'   : 'http://localhost:8080/auth/facebook/callback'
+    },
+    'twitterAuth' : {
+        'consumerKey'       : 'ElH1u1V6wvejpcA03ip6nGbjG',
+        'consumerSecret'    : 'uBbCG85BnT5IyU4WtPVW3s6VUsBI56asEzMWY9I1rpBHooEwAG',
+        'callbackURL'       : 'http://localhost:8080/auth/twitter/callback'
+    },
+    'googleAuth' : {
+        'clientID'      : '711780751348-bhedpvunp4l0jtepqqc6rcto7l7878dg.apps.googleusercontent.com',
+        'clientSecret'  : 'sRyfTMUvmAu-NdQYg13tNbRP',
+        'callbackURL'   : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
---
+++ config/database.js
@@ -0,0 +1,3 @@
+module.exports = {
+    'url' : 'mongodb://localhost/nodeAuth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+};

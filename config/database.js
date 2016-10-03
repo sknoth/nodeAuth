2a139df2079288f730416db4ab75009c0a634e1f
Stefanie Knoth <Stefanie Knoth>
2016-10-03 18:28:23 +0200

    changed stuff

--- config/database.js
+++ config/database.js
@@ -1,4 +1,4 @@
 module.exports = {
-    'url' : 'mongodb://localhost/nodeAuth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
-    // 'url': 'mongodb://admin:admin@ds023245.mlab.com:23245/nodeauth'
+    // 'url' : 'mongodb://localhost/nodeAuth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'url': 'mongodb://admin:admin@ds023245.mlab.com:23245/nodeauth'
 };
